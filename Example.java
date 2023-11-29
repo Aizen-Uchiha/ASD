8e2b574da28713c87258a5bfdfbe24a13057ef1e
Aizen-Uchiha <127953644+Aizen-Uchiha@users.noreply.github.com>
2023-11-29 20:44:24 +0800

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,19 @@
+class Example {
+  
+
+  static void fun(int n)
+  {
+    if (n > 0) 
+    {
+      System.out.print(n + " ");
+
+      fun(n - 1);
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    int x = 3;
+    fun(x);
+  }
+}
